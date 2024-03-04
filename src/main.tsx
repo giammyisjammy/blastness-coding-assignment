cb4260f3b98dd43e08cacae6c541cd1f2a4f324d
giammyisjammy <49064602+giammyisjammy@users.noreply.github.com>
2024-03-04 23:26:25 +0100

    ⚰️ Remove dead code

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,6 @@ import { CssBaseline } from '@mui/material';
 import App from './App';
 import theme from './theme';
 
-import './index.css';
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>

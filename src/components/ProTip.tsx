a45a3a07a4a20389d26a19ac2c4e39aff6b3e4ba
giammyisjammy <49064602+giammyisjammy@users.noreply.github.com>
2024-03-04 23:27:06 +0100

    💄 Update style and look

--- src/components/ProTip.tsx
+++ src/components/ProTip.tsx
@@ -14,11 +14,9 @@ export default function ProTip() {
   return (
     <Typography sx={{ mt: 6, mb: 3, color: 'text.secondary' }}>
       <LightBulbIcon sx={{ mr: 1, verticalAlign: 'middle' }} />
-      {'Pro tip: See more '}
-      <Link href="https://mui.com/material-ui/getting-started/templates/">
-        templates
-      </Link>
-      {' in the Material UI documentation.'}
+      {'Pro tip: using '}
+      <Link href="https://github.com/gvergnaud/ts-pattern">ts-pattern</Link>
+      {' to write better and safer conditions.'}
     </Typography>
   );
 }
